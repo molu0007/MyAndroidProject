1fc6286c76a9e40b5746c5d1459be6e27bfd5b60
molu0007 <1205028957@qq.com>
2018-06-14 18:09:33 +0800

    框架搭建

--- commonlibrary/src/main/java/com/framework/android/my/commonlibrary/BaseActivity.java
+++ commonlibrary/src/main/java/com/framework/android/my/commonlibrary/BaseActivity.java
@@ -1,13 +1,51 @@
 package com.framework.android.my.commonlibrary;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 import com.framework.android.my.commonlibrary.mvp.presenter.BasePresenter;
 import com.framework.android.my.commonlibrary.mvp.view.IView;
+import com.framework.android.my.commonlibrary.views.empty.EmptyLayout;
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
+
+import javax.inject.Inject;
 
 /**
  * @author dengwg
  * @date 2018/6/13
  */
 public  abstract class BaseActivity<T, P extends BasePresenter> extends RxAppCompatActivity implements IView<T> {
+    /**
+     * 加载状态布局
+     */
+    private EmptyLayout mEmptyLayout;
+    @Nullable
+    @Inject
+    protected P presenter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (isNeedTitleLayout()){
+
+        }
+    }
+
+    /**
+     * 是否需要标题栏
+     * @return
+     */
+    protected abstract boolean isNeedTitleLayout();
+    /**
+     * 是否需要标题栏
+     * @return
+     */
+    protected abstract boolean isNeedEmptyLayout();
+
+    protected abstract int getContentLayout();
+
+    protected abstract void initView();
+
+    protected abstract void initData();
 
 }
